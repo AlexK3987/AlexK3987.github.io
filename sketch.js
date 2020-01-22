1d3d96f3ba49f9795a3cf43bb5c928487e354b63
Alexander Kutner <ak3987@columbia.edu>
2020-01-22 15:27:26 -0500

    Final draft 1

--- sketch.js
+++ sketch.js
@@ -8,31 +8,40 @@ function draw() {
   background(225);
   textSize(50);
   fill(180);
-  text(hour(), 10, 30);
+  //text(hour(), 10, 30);
   fill(100);
-  text(minute(), 10, 60);
+  //text(minute(), 10, 60);
   fill(0);
-  text(second(), 10, 90);
-
-  let hr = map(hour(),0,23,150,400)
+  //text(second(), 10, 90);
+  
+  let hr = map(hour(),0,23,100,400)
   let mn = map(minute(),0,59, 50, hr)
   let sec = map(second(),0,59, 1, mn)
   fill(0)
   ellipse(200,200,400,400)
-  let hrs_left=150;
-
+  let hrs_left=100;
+  
   while(hrs_left<400){
     noFill();
     stroke(170);
     ellipse(200,200,hrs_left,hrs_left);
-    hrs_left+=200/24;
+    hrs_left+=300/23;
   }
-
-  let mins_left=100;
-
+  
+  
+  
   stroke(0);
   fill(220);
   ellipse(200,200,hr,hr);
+  let mins_left=50;
+  while(mins_left<hr){
+    noFill();
+    stroke(250)
+    ellipse(200,200,mins_left,mins_left);
+    mins_left+=(hr-50)/4
+    
+  }
+  stroke(0);
   fill(190);
   ellipse(200,200,mn,mn);
   fill(150);
@@ -41,4 +50,5 @@ function draw() {
     print("The minute has changed from " + last_min + " to " + minute());
     last_min=minute();
   }
+  
 }
