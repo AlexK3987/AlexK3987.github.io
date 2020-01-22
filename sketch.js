49f7e09db44ff94edf1cc4df9a1fdc6739d90066
Alexander Kutner <ak3987@columbia.edu>
2020-01-22 16:48:24 -0500

    smooth second circle

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
 var last_min;
+var ms;
+var prev_ms;
 function setup() {
   createCanvas(400, 400);
   last_min=minute();
+  ms=second()*1000;
+  prev_ms=millis()
 }
 
 function draw() {
@@ -14,10 +18,14 @@ function draw() {
   fill(0);
   //text(second(), 10, 90);
   
+  ms+=(millis()-prev_ms)%60000
+  prev_ms=millis()
+  
   let hr = map(hour(),0,23,100,400)
   let mn = map(minute(),0,59, 50, hr)
   let sec = map(second(),0,59, 1, mn)
-  fill(0)
+  let mill = map(ms,0,60000,1,mn)
+  
   ellipse(200,200,400,400)
   let hrs_left=100;
   
@@ -34,21 +42,28 @@ function draw() {
   fill(220);
   ellipse(200,200,hr,hr);
   let mins_left=50;
+
+  stroke(0);
+  fill(190);
+  ellipse(200,200,mn,mn);
+  
+  fill(150);
+  
   while(mins_left<hr){
     noFill();
-    stroke(250)
+    stroke(170)
     ellipse(200,200,mins_left,mins_left);
     mins_left+=(hr-50)/4
     
   }
-  stroke(0);
-  fill(190);
-  ellipse(200,200,mn,mn);
-  fill(150);
-  ellipse(200,200,sec,sec);
+  stroke(0)
+  //ellipse(200,200,sec,sec);
+  ellipse(200,200,mill,mill);
   if(last_min!=minute()){
     print("The minute has changed from " + last_min + " to " + minute());
     last_min=minute();
+    ms=0
   }
   
 }
+
